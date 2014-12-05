9f73b8003e18abd2ef8e882427954f8e67de7ae8
blackmode <t.slunsky@gmail.com>
2014-12-05 09:52:13 +0100

    Neco

---
+++ Makefile
@@ -0,0 +1,17 @@
+CC = gcc
+CFLAGS = -Wall -g
+
+all:	sim
+
+sim.o : sim.c
+	${CC} ${CFLAGS} -c sim.c
+
+run:
+	./sim
+
+pack:
+	zip 07_xslun01_xstefl05.zip Makefile dokumentace.pdf *.h *.c 	
+
+clean:
+	rm -f sim
+	rm -f sim.o
---
+++ sim.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// MAKRA
+
+
+// deklarace fci
+
+
+// FCE
+int vypocetVydelkuZaRok () {
+
+	return 0;
+}
+
+int procentualniPrirustekLodiZaRok () {
+
+	return 0;
+}
+
+
+int main () {
+	/**DEFINOVÁNÍ VSTUPŮ*/
+	// volani fci
+
+	printf("hello\n");
+	return 0;
+}

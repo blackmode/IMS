d23cb52fe57e5d062c36ab494c07885c2d6258e6
Lubik <lubomirstefl@gmail.com>
2014-12-05 18:49:20 +0100

    neco malo

--- sim.c
+++ sim.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
 // MAKRA v milionech Euro
-float prvni_etapa = 892.2;
-#define PRVNI_ETAPA 000   	//892.2
+const float prvni_etapa = 892.20;
+const float prvnia_etapa = 400.90;
+const float druha_etapa = 763.30;
+const float treti_etapa = 1992.20;
+const float ctvrta_etapa = 4106.90;
+/*#define PRVNI_ETAPA 000   	//892.2
 #define DRUHA_ETAPA 000		//763.3
 #define TRETI_ETAPA 000		//1992.2
 #define PRVNIA_ETAPA 000 	//400.9
-#define CTVRTA_ETAPA 000 	//4106.9
+#define CTVRTA_ETAPA 000 	//4106.9*/
+//volitelna simulace na urcity pocet let od roku 2032
+//zadame kolik let a je to
+//defaultne na 20
+int delka_simulace = 20;
+int rok = 2033;
+
+//hustota prepravy na jednotlovych kanalech za rok
+float hustota_dunaj = 30.316;
+float hustota_odra = 19.859;
+float hustota_labe = 4.675;
+
+
 // deklarace fci
-
-
+float narustek = 1;
 // funkce
 int vypocetVydelkuZaRok () {
 
@@ -18,15 +33,58 @@ int vypocetVydelkuZaRok () {
 // sem bych zadaval ruzny přisrustek ptz tu je 1% ale kdyz se kouknes jak to jinde 
 //tak bych to udelal ruzne
 int procentualniPrirustekLodiZaRok () {
-
+	//narustek o kolik procent lodni doprava zhoustla zadan v procentech
+	narustek = 1.01;
+	hustota_dunaj *= narustek;
+	hustota_odra *= narustek;
+	hustota_labe *= narustek;
 	return 0;
 }
-  
+
+//zjistuje zda jsou rekonstrukce v danem roce
+float rekonstrukceEtap(){
+	float castka_rekonstrukce = 0;
+
+	if(rok == 2036){
+		castka_rekonstrukce = prvni_etapa*0.15;
+	}
+	if(rok == 2038){
+		castka_rekonstrukce = druha_etapa*0.15;
+	}
+	if(rok == 2042){
+		castka_rekonstrukce = treti_etapa*0.15;
+	}
+	if(rok == 2043){
+		castka_rekonstrukce = prvnia_etapa*0.15;
+	}
+	if(rok == 2048){
+		castka_rekonstrukce = ctvrta_etapa*0.15;
+	}
+
+	return castka_rekonstrukce;
+}
 
 int main () {
+	int i;
+	float rekonstrukce = 0;
+	//cyklus simulace
+	for(i = 0; i < delka_simulace; i++){
+
+		rekonstrukce = rekonstrukceEtap();
+
+		//printf("rok %d a castka rek %.2f\n", rok, rekonstrukce );
+		/*printf("hustota dopravy na dunajske vetvi %.3f\n", hustota_dunaj);
+		printf("hustota dopravy na oderske vetvi %.3f\n", hustota_odra);
+		printf("hustota dopravy na labske vetvi %.3f\n\n", hustota_labe);*/
+		printf("%.3f  %.3f  %.3f\n",hustota_dunaj, hustota_odra, hustota_labe );
+		procentualniPrirustekLodiZaRok();
+		rok++;
+	}
+
 	/**DEFINOVÁNÍ VSTUPŮ*/
 	// volani fci
-	printf("%f", prvni_etapa);
-	printf("hello\n");
+	//prvni_etapa_udrzba = prvni_etapa*0.2;
+	//printf("%.2f\n", prvni_etapa_udrzba);
+	//printf("hello\n");
 	return 0;
 }
